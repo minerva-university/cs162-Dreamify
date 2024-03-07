15b9c1a435c61d58b966cd181943e341fcb045d8
AIconicInsight <Franek.Paul@gmx.de>
2024-03-07 17:01:40 +0100

    adjusted App.js so it is compatible with PR #13

--- src/App.js
+++ src/App.js
@@ -1,34 +1,68 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import "./App.css";
-import LandingPage from "./pages/LandingPage";
-import UserProfile from "./pages/UserProfile";
-import Header from "./components/Header";
 import { Container } from "react-bootstrap";
-import NewStoryPage from "./pages/NewstoryPage";
-import LoginPage from "./pages/LoginPage";
-import HomePage from "./pages/HomePage";
-import BedtimeStory from "./pages/StoryPage";
+
+// Styles
+import "./App.css";
+
+// Contexts
 import ApiProvider from "./contexts/ApiProvider";
 import AuthProvider from "./contexts/AuthProvider";
+
+// Components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+// Pages
+import HomePage from "./pages/HomePage";
+import UserProfilePage from "./pages/UserProfilePage";
+import AddachildPage from "./pages/AddachildPage";
+import LibraryPage from "./pages/LibraryPage";
+import NewStoryPage from "./pages/NewstoryPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import StoryPage from "./pages/StoryPage";
 
 export default function App() {
   return (
     <Container fluid className="App">
       <BrowserRouter>
-        <Header />
+        {/* <Header />  Tamirlan (make sure to have two variants with a prop */}
         <ApiProvider>
           <AuthProvider>
             <Routes>
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/home" element={<HomePage />} />
-              <Route path="/user-profile" element={<UserProfile />} />
-              <Route path="/new-story" element={<NewStoryPage />} />
-              <Route path="/story" element={<BedtimeStory />} />
+              <Route path="/" element={<HomePage />} /> {/* Tamirlan */}
+              <Route
+                path="/myprofile/:parentid"
+                element={<UserProfilePage />}
+              />{" "}
+              {/* Billy */}
+              <Route
+                path="/addachild/:parentid"
+                element={<AddachildPage />}
+              />{" "}
+              {/* Billy  */}
+              <Route
+                path="/library/story/:storyid"
+                element={<StoryPage />}
+              />{" "}
+              {/* MISHA */}
+              <Route
+                path="/library/parent/:parentid"
+                element={<LibraryPage />}
+              />{" "}
+              {/* MISHA */}
+              <Route
+                path="/newstory/:childid"
+                element={<NewStoryPage />}
+              />{" "}
+              {/* MISHA */}
               <Route path="/login" element={<LoginPage />} />
+              {/* Flambeau */}
+              <Route path="/signup" element={<SignupPage />} /> {/* Flambeau */}
             </Routes>
           </AuthProvider>
         </ApiProvider>
+        {/* <Footer /> {/* Tamirlan  */}
       </BrowserRouter>
     </Container>
   );
