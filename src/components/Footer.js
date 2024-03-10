69d524c737ba60639976ac6472f40f0fb4c9ff43
tbeka <tbektemissov@gmail.com>
2024-03-10 12:47:34 +0100

    Added footer and header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import styles from '/styles/Footer.css'; // Ensure the path to your CSS module is correct
 
-// Tamirlan
 export default function Footer() {
   return (
-    <h1>Footer!</h1>
+    <Container fluid className={styles.Footer_6_75}>
+      <Row className="justify-content-center">
+        <Col xs={12}lg={2} className="text-center">
+          <span className={styles.Dreamify_6_76}>&#xA9;2024 Dreamify</span>
+        </Col>
+        <Col xs={12} lg={3} className="text-center">
+          <span className={styles.DreamifyMinervaGmailCom_6_79}>dreamify.minerva@gmail.com</span>
+        </Col>
+        <Col xs={12} lg={2}></Col>
+        <Col xs={12} lg={2} className="text-center">
+          <a href="#aboutus" className={`${styles.AboutUs_6_77} text-decoration-none`}>About Us</a>
+        </Col>
+        <Col xs={12} lg={3} className="text-center">
+          <a href="#terms" className={`${styles.TermsOfServicePrivacyPolicy_6_78} text-decoration-none`}>Terms of Service & Privacy Policy</a>
+        </Col>
+      </Row>
+    </Container>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import styles from '../styles/Header.module.css'; // Adjust the path as necessary
 
-// Tamirlan
-export default function Header() {
+export default function Page() {
   return (
-    <h1>Header!</h1>
+    <Navbar expand="lg" className={styles.Header_2_2}>
+      <Container>
+        <Navbar.Brand href="#home" className={styles.Dreamify_2_4}>Dreamify</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className={`me-auto ${styles.NavLinksContainer}`}> {/* Adjust alignment and gap here */}
+            <div className={styles.Rectangle_2_2_3}> {/* Center content */}
+              <Nav.Link href="#home" className={styles.Home_2_5}>Home</Nav.Link>
+            </div>
+            <div className={styles.Rectangle_3_2_6}> {/* Center content */}
+              <Nav.Link href="#get-started" className={styles.GetStarted_2_7}>Get Started</Nav.Link>
+            </div>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
