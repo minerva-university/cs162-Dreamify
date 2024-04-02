967e28e391e11e77168da2fbc92038f7e6ea1961
AIconicInsight <Franek.Paul@gmx.de>
2024-04-02 12:54:40 +0200

    UserProfilePage.js: fixed account info box width and changed username to first and last name instead, as we don't have a username

--- src/pages/UserProfilePage.js
+++ src/pages/UserProfilePage.js
@@ -74,7 +74,8 @@ export default function UserProfilePage() {
         <h2>Account Information</h2>
         <div className="account-info-list">
           <div className="account-info-item">
-            <strong>USERNAME</strong> {userInfo.first_name}
+            <strong>Name</strong>{" "}
+            {`${userInfo.first_name} ${userInfo.last_name}`}
           </div>
           <div className="account-info-item">
             <strong>Email</strong> {userInfo.email}
