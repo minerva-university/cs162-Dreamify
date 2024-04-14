0179798c1af8d84a0b424b9f8060508f9f90cded
Billy Altangerel <bilegjargal@uni.minerva.edu>
2024-04-14 22:17:55 +0800

    A small abstraction on add/modify child

---
+++ src/components/ChildAttributes.js
@@ -0,0 +1,51 @@
+export const eyeColors = [
+    { name: "Blue", imageUrl: require("../assets/add_child_pics/image 9.jpg") },
+    { name: "Brown", imageUrl: require("../assets/add_child_pics/image 10.jpg") },
+    { name: "Green", imageUrl: require("../assets/add_child_pics/image 11.jpg") },
+    { name: "Hazel", imageUrl: require("../assets/add_child_pics/image 12.jpg") },
+    { name: "Amber", imageUrl: require("../assets/add_child_pics/image 13.jpg") },
+    { name: "Gray", imageUrl: require("../assets/add_child_pics/image 14.jpg") },
+  ];
+  
+export const hairType = [
+    {
+      name: "Straight",
+      imageUrl: require("../assets/add_child_pics/image 20.jpg"),
+    },
+    { name: "Wavy", imageUrl: require("../assets/add_child_pics/image 21.jpg") },
+    { name: "Curly", imageUrl: require("../assets/add_child_pics/image 22.jpg") },
+    { name: "Kinky", imageUrl: require("../assets/add_child_pics/image 23.jpg") },
+    { name: "Bald", imageUrl: require("../assets/add_child_pics/image 24.jpg") },
+  ];
+  
+export  const hairColor = [
+    {
+      name: "Blonde",
+      imageUrl: require("../assets/add_child_pics/image 26.jpg"),
+    },
+    { name: "Brown", imageUrl: require("../assets/add_child_pics/image 27.jpg") },
+    { name: "Black", imageUrl: require("../assets/add_child_pics/image 28.jpg") },
+    { name: "Red", imageUrl: require("../assets/add_child_pics/image 29.jpg") },
+    {
+      name: "Auburn",
+      imageUrl: require("../assets/add_child_pics/image 30.jpg"),
+    },
+    { name: "Gray", imageUrl: require("../assets/add_child_pics/image 31.jpg") },
+    { name: "White", imageUrl: require("../assets/add_child_pics/image 32.jpg") },
+  ];
+  
+export const races = [
+    { name: "Asian", imageUrl: require("../assets/add_child_pics/image 45.jpg") },
+    { name: "Black", imageUrl: require("../assets/add_child_pics/image 46.jpg") },
+    { name: "Brown", imageUrl: require("../assets/add_child_pics/image 47.jpg") },
+    { name: "White", imageUrl: require("../assets/add_child_pics/image 48.jpg") },
+    {
+      name: "Hispanic",
+      imageUrl: require("../assets/add_child_pics/image 49.jpg"),
+    },
+    {
+      name: "Middle Eastern",
+      imageUrl: require("../assets/add_child_pics/image 50.jpg"),
+    },
+  ];
+  
--- src/pages/AddachildPage.js
+++ src/pages/AddachildPage.js
@@ -1,56 +1,10 @@
 import React, { useState, useEffect} from "react";
 import { useApi } from "../contexts/ApiProvider";
 import { useNavigate } from "react-router-dom";
+import { eyeColors, hairType, hairColor, races } from '../components/ChildAttributes.js';
 
 import Spinner from "../components/Spinner";
 import "./styles/AddachildPage.css";
-
-const eyeColors = [
-  { name: "Blue", imageUrl: require("../assets/add_child_pics/image 9.jpg") },
-  { name: "Brown", imageUrl: require("../assets/add_child_pics/image 10.jpg") },
-  { name: "Green", imageUrl: require("../assets/add_child_pics/image 11.jpg") },
-  { name: "Hazel", imageUrl: require("../assets/add_child_pics/image 12.jpg") },
-  { name: "Amber", imageUrl: require("../assets/add_child_pics/image 13.jpg") },
-  { name: "Gray", imageUrl: require("../assets/add_child_pics/image 14.jpg") },
-];
-
-const hairType = [
-  {
-    name: "Straight",
-    imageUrl: require("../assets/add_child_pics/image 20.jpg"),
-  },
-  { name: "Wavy", imageUrl: require("../assets/add_child_pics/image 21.jpg") },
-  { name: "Curly", imageUrl: require("../assets/add_child_pics/image 22.jpg") },
-  { name: "Kinky", imageUrl: require("../assets/add_child_pics/image 23.jpg") },
-  { name: "Bald", imageUrl: require("../assets/add_child_pics/image 24.jpg") },
-];
-
-const hairColor = [
-  {
-    name: "Blonde",
-    imageUrl: require("../assets/add_child_pics/image 26.jpg"),
-  },
-  { name: "Brown", imageUrl: require("../assets/add_child_pics/image 27.jpg") },
-  { name: "Black", imageUrl: require("../assets/add_child_pics/image 28.jpg") },
-  { name: "Red", imageUrl: require("../assets/add_child_pics/image 29.jpg") },
-  {
-    name: "Auburn",
-    imageUrl: require("../assets/add_child_pics/image 30.jpg"),
-  },
-  { name: "Gray", imageUrl: require("../assets/add_child_pics/image 31.jpg") },
-  { name: "White", imageUrl: require("../assets/add_child_pics/image 32.jpg") },
-];
-
-const races = [
-  { name: "Asian", imageUrl: require("../assets/add_child_pics/image 45.jpg") },
-  { name: "Black", imageUrl: require("../assets/add_child_pics/image 46.jpg") },
-  { name: "Pacific islander", imageUrl: require("../assets/add_child_pics/image 47.jpg") },
-  { name: "White", imageUrl: require("../assets/add_child_pics/image 48.jpg") },
-  {
-    name: "Hispanic or Latino",
-    imageUrl: require("../assets/add_child_pics/image 49.jpg"),
-  },
-];
 
 const AddachildPage = () => {
 
